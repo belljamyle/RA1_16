e9cf56cd16f8a374f17d5f0f44b18beed29ede42
bell <128303802+belljamyle@users.noreply.github.com>
2026-03-27 00:42:44 -0300

    gerador de assembly

--- main.py
+++ main.py
@@ -72,10 +72,84 @@ def tokenGenerator(line):
     return tokens
 
 
+def assemblyGenerator(token, assembly):
+    pile = []
+
+    recorder = 0
+    label_id = 0
+
+    results = []
+    memory = 0
+
+    for type, value in token:
+        if type == "NUMBER":
+            rec = "R" + str(recorder)
+            assembly.append("MOV " + rec + ", #" + str(value))
+            pile.append(rec)
+            recorder += 1
+
+        elif type == "OP":
+            b = pile.pop()
+            a = pile.pop()
+
+            if value == "+":
+                assembly.append("ADD " + a + ", " + a + ", " + b)
+            elif value == "-":
+                assembly.append("SUB " + a + ", " + a + ", " + b)
+            elif value == "*":
+                assembly.append("MUL " + a + ", " + a + ", " + b)
+            elif value == "/":
+                assembly.append("SDIV " + a + ", " + a + ", " + b)
+            elif value == "%":
+                assembly.append("MOD " + a + ", " + a + ", " + b)
+            elif value == "^":
+                label_id += 1
+                loop_label = f"loop_{label_id}"
+                end_label = f"end_{label_id}"
+
+                resultado = f"R{recorder}"
+                recorder += 1
+
+                # resultado = base (a)
+                assembly.append(f"MOV {resultado}, {a}")
+
+                # expoente-- (b = b - 1)
+                assembly.append(f"SUB {b}, {b}, #1")
+
+                # loop
+                assembly.append(f"{loop_label}:")
+                assembly.append(f"CMP {b}, #0")
+                assembly.append(f"BEQ {end_label}")
+                assembly.append(f"MUL {resultado}, {resultado}, {a}")
+                assembly.append(f"SUB {b}, {b}, #1")
+                assembly.append(f"B {loop_label}")
+                assembly.append(f"{end_label}:")
+
+                pile.append(resultado)
+                continue
+            elif value == "//":
+                assembly.append("SDIV " + a + ", " + a + ", " + b)
+
+            pile.append(a)
+
+        elif type == "MEM":
+            if pile:
+                value = pile.pop()
+                assembly.append("MOV R_MEM, " + value)
+                memory = "R_MEM"
+            else:
+                pile.append(memory)
+
+    return assembly
+
+
 archive = sys.argv[1]
+
+assembly = []
 
 with open(archive, "r") as f:
     for line in f:
         print(line.strip())
         token = tokenGenerator(line.strip())
+        assemblyGenerator(token, assembly)
         print(token)
